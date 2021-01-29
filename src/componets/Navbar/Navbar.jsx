2fce6248a36cdb239c74b85bde7e0b1f551bd931
ForeverMikk <michaelmolina679@gmail.com>
2021-01-29 15:35:19 -0600

    Second Update Just a few newthings

--- src/componets/Navbar/Navbar.jsx
+++ src/componets/Navbar/Navbar.jsx
@@ -2,15 +2,46 @@
 // No es necesario puedes hacerlo con los puros signos sin abrir nada
 import React from 'react';
 import logo from './../../assets/img/logo.png';
+import github from './../../assets/img/github.png';
+import twitter from '../../assets/img/twitter.png'
+
 import './navbar.css';
 
 const PrimeraApp = () => { //Componente o Funcional Component
     return (
-        <div>
-            <img src={logo} alt=""/>
-            <h1>Hola Mundo</h1>
+        <div className='nav-container'>
+            <header>
+                <img src={logo} alt=""/>    
+                <nav>
+                    <ul>
+                        <li><a href="#">About Me</a></li>
+                        <li><a href="#">My Work</a></li>
+                        <li><a href="#">Services</a></li>
+                        <li><a href="#">Contact Me</a></li>
+                    </ul>
+                </nav>
+            </header>
+            <div className='social-header'>
+                <ul>
+                    <li><a href="#"><img src={github} alt=""/></a></li>
+                    <li><a href="#"><img src={twitter} alt=""/></a></li>
+                </ul>
+            </div>         
         </div>
     )
 }
 
+const Home = () => {
+    return (
+        <div className="hero">
+            <div className="content">
+                <p>Hello, My Name is</p>
+                <h1>Michael Molina</h1>
+            </div>
+        </div>
+    )
+}
+
+
 export default PrimeraApp;
+export default Home;
