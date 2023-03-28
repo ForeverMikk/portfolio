49f7f6ba68936d8a9f810f45a3a212b00b69dcf1
unknown <michaelmolina679@gmail.com>
2023-03-28 13:34:11 -0600

    new title

--- src/componets/Home/Home.jsx
+++ src/componets/Home/Home.jsx
@@ -30,7 +30,7 @@ export const Home = () => {
           <div className='home-data'>
             <span className='home-text'>Nice to meet you</span>
             <h1 className='home-name'>
-              I am <span>Michael Molina</span>
+              Hola que hace I am <span>Michael Molina</span>
             </h1>
 
             <div className='buttons'>
